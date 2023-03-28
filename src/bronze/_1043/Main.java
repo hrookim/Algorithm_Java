4bd388ef71a716d5b17d4e79215d5591d8823b28
김혜림 <hrookim@gmail.com>
2023-03-28 17:55:37 +0900

    [ongoing] BOJ_1043

---
+++ src/bronze/_1043/Main.java
@@ -0,0 +1,21 @@
+package bronze._1043;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+//		인원 수, 파티 수
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		
+	}
+
+
+
+}
